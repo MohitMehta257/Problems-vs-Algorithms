90c462a977db11154497126d34ce743f44b98b76
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2020-05-16 21:14:10 +0530

    Create problem_4.py

---
+++ problem_4.py
@@ -0,0 +1,37 @@
+def mergesort(items):
+
+    if len(items) <= 1:
+        return items
+    
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+    
+    left = mergesort(left)
+    right = mergesort(right)
+    
+    return merge(left, right)
+    
+def merge(left, right):
+    
+    merged = []
+    left_index = 0
+    right_index = 0
+    
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+        
+    return merged
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
